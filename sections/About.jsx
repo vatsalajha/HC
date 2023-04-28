3fb4a28fe8bcbfe4b056f102880c1c2304feee35
Vatsala <jhavatsala@gmail.com>
2023-04-28 15:44:30 +0545

    Add files via upload

---
+++ sections/About.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Us" textStyles="text-left" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        We're dedicated to leveraging cutting-edge <span className="font-extrabold text-white">blockchain technology </span>
+        to build innovative and transparent solutions for <span className="font-extrabold text-white"> land registry management </span>
+        and <span className="font-extrabold text-white"> government fund tracking </span>, driving positive
+        change and ensuring accountability.
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;

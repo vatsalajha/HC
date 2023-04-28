7be44c21c974c48f80d35579f15f9feb1142e656
Vatsala <jhavatsala@gmail.com>
2023-04-28 15:34:43 +0545

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,105 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/R&DSemiconductor.png',
+    title: 'Semiconductor Plant',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/Solar&WindPark.png',
+    title: 'Renewable Energy Solutions',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/HRD.png',
+    title: 'Skill Development',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/PlantManufacturing.png',
+    title: 'Manufacturing Plant',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/CultureCenter.png',
+    title: 'Cultural Center Development',
+  },
+  {
+    id: 'world-6',
+    imgUrl: '/CityDev.png',
+    title: 'New City Development',
+  },
+  // {
+  //   id: 'world-7',
+  //   imgUrl: '/fibercable.png',
+  //   title: 'Fiber Cable',
+  // },
+  // {
+  //   id: 'world-8',
+  //   imgUrl: '/forest.png',
+  //   title: 'Afforestation',
+  // },
+];
+
+export const startingFeatures = [
+  'Project Announced -> Government Allocates Funds',
+  'Allocated amount displayed on blockchain',
+  'Tender process for procuring raw materials or services is also recorded on the blockchain',
+  'Funds are disbursed through the smart contract, only released when the predefined conditions are met.',
+  'Expense reports and bills submitted by the contractor are verified by independent third parties.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'The transparency and immutability of the blockchain make it difficult for corrupt officials to hide their actions or manipulate the records.',
+  },
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'End Goal',
+    subtitle:
+        'If any corrupt practices are detected during the verification and audit process, the responsible individuals can be held accountable and prosecuted.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/CityDev.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/fibercable.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/forest.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];

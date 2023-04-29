992eb550738477e765e7ff352519e2c7f01adbe1
shashwat-v <shashwat2775@gmail.com>
2023-04-29 14:22:30 +0530

    added metamask functionality and removed the node modules

--- .next/server/flight-server-css-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_CSS_MANIFEST={"__entry_css__":{"C:\\Users\\SHASHWAT\\Desktop\\HC\\app\\page":["C:\\Users\\SHASHWAT\\Desktop\\HC\\styles\\globals.css"]},"C:\\Users\\SHASHWAT\\Desktop\\HC\\app\\layout.js":["C:\\Users\\SHASHWAT\\Desktop\\HC\\styles\\globals.css"]}
